849b3dc718dc7746ea786c592dae3c4d09c6073b
Rasracapur <57629292+Rasracapur@users.noreply.github.com>
2020-02-20 15:05:42 -0300

    alterações na página

--- server.js
+++ server.js
@@ -5,18 +5,16 @@ const server = express()
 //Configurar o servidor para mostrar arquivos extras
 server.use(express.static("public"))
 
-//Ligar o servidor e permitir que use a porta 3000
-server.listen(3000, () => {
-  console.log("Iniciei o servidor...")
-})
-
+//Habilitar o body no formulário
+server.use(express.urlencoded({extended: true}))
 
 //Configurando o template
 const nunjucks = require('nunjucks')
 nunjucks.configure("./",{
-  autoescape: true,
-  express: server
+  express: server,
+  noCache: true
 })
+
 
 //Lista de Doadores: Vetor ou Array
 const donors = [
@@ -40,7 +38,18 @@ const donors = [
 
 //Configurar a apresentação da página
 server.get("/", (req, res) => {
-  return res.render("./public/index.html", {valor: "1"})
+  return res.render("./index.html", { donors })
 })
 
+server.post("/", function (req, res) {
+  //Pegar os dados do formulário
+  const name = req.body.name
+  const email = req.body.email
+  const blood = req.body.blood
+})
+
+//Ligar o servidor e permitir que use a porta 3000
+server.listen(3000, () => {
+  console.log("Iniciei o servidor...")
+})
 
